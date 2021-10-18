089fc07e2201115715beb398c8f7ba59888cac56
mokchira <mokchira@gmail.com>
2021-10-18 00:32:21 -0400

    Revert "Revert "Update""
    This reverts commit 6af1b8251127358ccadfd8ff43ff7c1b42788296.

--- src/include/coal/intersect.h
+++ src/include/coal/intersect.h
@@ -7,3 +7,5 @@ bool coal_PointInCircle(Coal_Vec2 point, Coal_Vec2 center, const float radius);
 int  coal_TriangleIntersect(Coal_Vec3 orig, Coal_Vec3 dir, Coal_Vec3 vert0,
                             Coal_Vec3 vert1, Coal_Vec3 vert2, float* t, float* u,
                             float* v);
+bool coal_AABBIntersect(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2);
+bool coal_PointInBox(float x, float y, float bx, float by, float bw, float bh);
